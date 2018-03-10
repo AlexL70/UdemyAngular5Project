c0afd9c6a51f940edb6a1db5868c7e4a7c20561d
AlexL70 <alexander.levinson.70@gmail.com>
2018-03-10 23:24:40 +0200

    22. Fully Understanding the Component Selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // tslint:disable-next-line:component-selector
+  // selector: '[app-servers]',
+  // tslint:disable-next-line:component-selector
+  // selector: '.app-servers',
   template: `
   <app-server></app-server>
   <app-server></app-server>
