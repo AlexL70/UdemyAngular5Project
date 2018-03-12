e15434ee19e314027a72c3985f5daf093423f1d8
AlexL70 <alexander.levinson.70@gmail.com>
2018-03-12 12:21:32 +0200

    Basic assignment 1; add styles for both components. Use internal style for WarningAlert and external style for SuccessAlert, just to practice both.

--- Assignments/basics-assignment-1/src/app/warning-alert/warning-alert.component.ts
+++ Assignments/basics-assignment-1/src/app/warning-alert/warning-alert.component.ts
@@ -3,7 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-warning-alert',
   templateUrl: './warning-alert.component.html',
-  styleUrls: ['./warning-alert.component.css']
+  styles: [`
+  div {
+    color: #8a6d3b;
+    background-color: #fcf8e3;
+    border-color: #faebcc;
+  }
+  `]
 })
 export class WarningAlertComponent implements OnInit {
 
