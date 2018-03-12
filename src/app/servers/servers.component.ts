30d1fd6553491f6e39beb81509bf5be1007c1f1e
AlexL70 <alexander.levinson.70@gmail.com>
2018-03-12 13:21:25 +0200

    24. String Interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]',
   // tslint:disable-next-line:component-selector
   // selector: '.app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
