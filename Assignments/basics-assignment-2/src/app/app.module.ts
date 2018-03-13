ae61cc2089f1a98f62e5b39f1e927ae3f5c7fbcf
AlexL70 <alexander.levinson.70@gmail.com>
2018-03-13 13:25:33 +0200

    Basic assignment 2; add UserName component

--- Assignments/basics-assignment-2/src/app/app.module.ts
+++ Assignments/basics-assignment-2/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { UserNameComponent } from './user-name/user-name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserNameComponent
   ],
   imports: [
     BrowserModule,
