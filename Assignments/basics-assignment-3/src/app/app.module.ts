4fba67841e1b28e60b86275491442621db0dcf15
AlexL70 <alexander.levinson.70@gmail.com>
2018-03-14 19:27:01 +0200

    Basic assignment 3; add logging-toggler component

--- Assignments/basics-assignment-3/src/app/app.module.ts
+++ Assignments/basics-assignment-3/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoggingTogglerComponent } from './logging-toggler/logging-toggler.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoggingTogglerComponent
   ],
   imports: [
     BrowserModule,
---
+++ Assignments/basics-assignment-3/src/app/logging-toggler/logging-toggler.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logging-toggler',
+  templateUrl: './logging-toggler.component.html',
+  styleUrls: ['./logging-toggler.component.css']
+})
+export class LoggingTogglerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
