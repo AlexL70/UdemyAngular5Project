a33f23acb3251138720d8f468f72d8b26982f3ec
AlexL70 <alexander.levinson.70@gmail.com>
2018-03-15 19:49:53 +0200

    49. Outputting a List of Recipes with ngFor

--- Assignments/basics-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Assignments/basics-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,8 +9,10 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is just a test recipe description',
+    'https://upload.wikimedia.org/wikipedia/commons/7/79/Goulash_from_usa.jpg'),
+    new Recipe('Second test recipe', 'This is just a second test recipe description',
     // tslint:disable-next-line:max-line-length
-    `https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fc%2Fcc%2FRecipe-575434.svg%2F1199px-Recipe-575434.svg.png&imgrefurl=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ARecipe-575434.svg&docid=H4ROo-OAOZiKPM&tbnid=6cJdALsSsjm86M%3A&vet=10ahUKEwif15DT6O7ZAhVI8WMKHUAED98QMwinAigDMAM..i&w=1199&h=1024&client=firefox-b-1-ab&bih=689&biw=1512&q=recipe&ved=0ahUKEwif15DT6O7ZAhVI8WMKHUAED98QMwinAigDMAM&iact=mrc&uact=8`)
+    'https://sftextures.com/texture/1829/0/1828/autumn-leaves-yellow-orange-red-og-green-grass-damaged-wetahered-beautiful-colorful-ground-seamless-texture-256x256.jpg')
   ];
 
   constructor() { }
