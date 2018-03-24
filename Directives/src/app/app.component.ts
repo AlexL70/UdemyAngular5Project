374c1a754173b8143d1648262c459d1493186870
AlexL70 <alexander.levinson.70@gmail.com>
2018-03-24 20:27:30 +0200

    83. ngFor and ngIf Recap

--- Directives/src/app/app.component.ts
+++ Directives/src/app/app.component.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1, 3, 5];
+  evenNumbers = [2, 4];
   onlyOdd = false;
+
+  getButtonText() {
+    if (this.onlyOdd) {
+      return 'Show only even numbers';
+    } else {
+      return 'Show only odd numbers';
+    }
+  }
 }
