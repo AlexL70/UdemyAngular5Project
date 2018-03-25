84f5a758ac97577e7b609f83ca31cb29f1144913
AlexL70 <alexander.levinson.70@gmail.com>
2018-03-25 14:25:27 +0300

    90. Binding to Directive Properties

--- Directives/src/app/better-highlight/better-highlight.directive.ts
+++ Directives/src/app/better-highlight/better-highlight.directive.ts
@@ -1,27 +1,35 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener,
+  HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
-  @HostBinding('style.color') color = 'black';
+  @Input() defaultBackgroundColor = 'transparent';
+  // tslint:disable-next-line:no-input-rename
+  @Input('appBetterHighlight') highlightBackgroundColor = 'blue';
+  @Input() defaultColor = 'black';
+  @Input() highlightColor = 'pink';
+  @HostBinding('style.backgroundColor') backgroundColor;
+  @HostBinding('style.color') color;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    this.backgroundColor = this.defaultBackgroundColor;
+    this.color = this.defaultColor;
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.backgroundColor = 'blue';
-    this.color = 'pink';
+    this.backgroundColor = this.highlightBackgroundColor;
+    this.color = this.highlightColor;
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
     // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'pink');
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.backgroundColor = 'transparent';
-    this.color = 'black';
+    this.backgroundColor = this.defaultBackgroundColor;
+    this.color = this.defaultColor;
     // this.renderer.setStyle(this.elRef.nativeElement, 'background', 'transparent');
     // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
   }
