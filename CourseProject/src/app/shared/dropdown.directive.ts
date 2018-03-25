a4019f8ab4b489b1e9473b83986feffaa8ab1c2c
AlexL70 <alexander.levinson.70@gmail.com>
2018-03-25 19:43:16 +0300

    94. Building and Using a Dropdown Directive (simple solution)

--- CourseProject/src/app/shared/dropdown.directive.ts
+++ CourseProject/src/app/shared/dropdown.directive.ts
@@ -1,17 +1,12 @@
-import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+  @HostBinding('class.open') isOpen = false;
 
-  @HostListener('click') mouseclick() {
-    const openCls = 'open';
-    if (this.elRef.nativeElement.classList.contains(openCls)) {
-      this.renderer.removeClass(this.elRef.nativeElement, openCls);
-    } else {
-      this.renderer.addClass(this.elRef.nativeElement, openCls);
-    }
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
   }
 }
