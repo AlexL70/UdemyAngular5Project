191dd733dff4df96aca9604b5ed5bfc5ef7a373a
AlexL70 <alexander.levinson.70@gmail.com>
2018-04-02 11:31:40 +0300

    108. Adding the Shopping List Service

--- CourseProject/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ CourseProject/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Output, ViewChild, ElementRef } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,15 +10,14 @@ import { Ingredient } from '../../shared/ingredient.model';
 export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('amountInput') amountInputRef: ElementRef;
-  @Output() newIngridientCreated = new EventEmitter<Ingredient>();
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
   addIngridient() {
-    this.newIngridientCreated.emit(new Ingredient(
-      this.nameInputRef.nativeElement.value,
-      this.amountInputRef.nativeElement.value));
+    this.shoppingListService.addIngredient(
+      new Ingredient(this.nameInputRef.nativeElement.value,
+        this.amountInputRef.nativeElement.value));
   }
 }
--- CourseProject/src/app/shopping-list/shopping-list.service.ts
+++ CourseProject/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,16 @@
-export class ShoppoingListService {
+import { Ingredient } from '../shared/ingredient.model';
 
+export class ShoppingListService {
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10)
+      ];
+
+    getIngridients(): Ingredient[] {
+        return this.ingredients.slice();
+    }
+
+    addIngredient(ingredient: Ingredient): void {
+        this.ingredients.push(ingredient);
+    }
 }
