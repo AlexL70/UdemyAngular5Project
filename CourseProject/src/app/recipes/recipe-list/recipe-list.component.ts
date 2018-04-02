8036b461adc2d24bb08b678042a655e327887a71
AlexL70 <alexander.levinson.70@gmail.com>
2018-04-02 09:31:43 +0300

    106. Managing Recipes in a Recipe Service

--- CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,17 +9,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is just a test recipe description',
-    'https://upload.wikimedia.org/wikipedia/commons/7/79/Goulash_from_usa.jpg'),
-    new Recipe('Second test recipe', 'This is just a second test recipe description',
-    // tslint:disable-next-line:max-line-length
-    'https://sftextures.com/texture/1829/0/1828/autumn-leaves-yellow-orange-red-og-green-grass-damaged-wetahered-beautiful-colorful-ground-seamless-texture-256x256.jpg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
--- CourseProject/src/app/recipes/recipe.service.ts
+++ CourseProject/src/app/recipes/recipe.service.ts
@@ -1,3 +1,16 @@
-export class RecipeService {
+import { Recipe } from './recipe.model';
 
+export class RecipeService {
+    private recipes: Recipe[] = [
+        new Recipe('A test recipe', 'This is just a test recipe description',
+        'https://upload.wikimedia.org/wikipedia/commons/7/79/Goulash_from_usa.jpg'),
+        new Recipe('Second test recipe', 'This is just a second test recipe description',
+        // tslint:disable-next-line:max-line-length
+        'https://sftextures.com/texture/1829/0/1828/autumn-leaves-yellow-orange-red-og-green-grass-damaged-wetahered-beautiful-colorful-ground-seamless-texture-256x256.jpg')
+      ];
+
+    getRecipes(): Recipe[] {
+        //  return copy so that recipe array cannot be changed from outside
+        return this.recipes.slice();
+    }
 }
