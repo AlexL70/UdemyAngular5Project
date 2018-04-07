7553aa4478e231707a73a0fad1b5843cc2d43a8e
AlexL70 <alexander.levinson.70@gmail.com>
2018-04-07 11:33:13 +0300

    124. An Important Note about Route Observables

--- Routing/src/app/users/user/user.component.ts
+++ Routing/src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -18,16 +20,23 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
-    //  "route.params" is an Observable. It is object, which fires an event
+    //  "route.params" is an Observable. It is an object, which fires an event
     //  whenever parameters change. So it could be used to move to the next
     //  object (user in this case) without actually recreate the object when
     //  just a parameter values change
-    this.route.params.subscribe(
+    this.paramsSubscription = this.route.params.subscribe(
       (params: Params) => {
         this.user.id = params.id;
         this.user.name = params.name;
       }
     );
+  }
+
+  //  This code is actually unnecessary. Angular will do it anyway for "params"
+  //  Observable. But if you create your own observables, it is a good idea to
+  //  clean up them after they are not needed anymore.
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
