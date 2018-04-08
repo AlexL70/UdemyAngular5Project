e3dceb68c8b91351871048b2c8635f551dddff22
AlexL70 <alexander.levinson.70@gmail.com>
2018-04-08 10:54:52 +0300

    131. Redirecting and Wildcard Routes

--- Routing/src/app/app.module.ts
+++ Routing/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent},
@@ -21,7 +22,12 @@ const appRoutes: Routes = [
   { path: 'servers', component: ServersComponent, children: [
     { path: ':id/edit', component: EditServerComponent },
     { path: ':id', component: ServerComponent }
-    ]},
+  ]},
+  { path: 'not-found', component: PageNotFoundComponent },
+  //  Thid must be the last route it simply redirects any unknown URL to
+  //  the 'not-found' page so that user see that path he/she entered
+  //  was not found
+  { path: '**', redirectTo: 'not-found'}
 ];
 
 @NgModule({
@@ -32,7 +38,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
