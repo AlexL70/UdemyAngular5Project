9141958eb62fc70ec301e3e29c4a0d165eaff886
AlexL70 <alexander.levinson.70@gmail.com>
2018-04-30 13:18:24 +0300

    Practicing Template-Driven Forms - add controls

--- Assignments/forms-td/src/app/app.component.ts
+++ Assignments/forms-td/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  readonly subscriptionTypes = ['Basic', 'Advanced', 'Pro'];
+  readonly defaultSubscription = 'Advanced';
 
+  onSubmit(): void {
+    console.log(`Submit!`);
+  }
 }
