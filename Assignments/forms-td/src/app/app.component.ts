e45c6051ffee6b37484709900e6d8cfcb4fa9816
AlexL70 <alexander.levinson.70@gmail.com>
2018-04-30 14:07:08 +0300

    Practicing Template-Driven Forms - add submit action

--- Assignments/forms-td/src/app/app.component.ts
+++ Assignments/forms-td/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('frm') signMeForm: NgForm;
   readonly subscriptionTypes = ['Basic', 'Advanced', 'Pro'];
   readonly defaultSubscription = 'Advanced';
+  submitted = false;
+
+  submitValue = { email: '', subscriptionType: '', password: ''};
 
   onSubmit(): void {
-    console.log(`Submit!`);
+    this.submitted = true;
+    this.submitValue.email = this.signMeForm.value.email;
+    this.submitValue.subscriptionType = this.signMeForm.value.subscriptionType;
+    // console.log(this.signMeForm);
+    this.submitValue.password = this.signMeForm.value.password;
+    this.signMeForm.form.reset({ subscriptionType: this.defaultSubscription});
   }
 }
