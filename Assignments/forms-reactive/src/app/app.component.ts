1717bf1fde81e08eb70f6218d0feeef1dc62dbf9
AlexL70 <alexander.levinson.70@gmail.com>
2018-05-06 21:51:09 +0300

    Assignment: Practicing Reactive Forms - add asynchronous validator

--- Assignments/forms-reactive/src/app/app.component.ts
+++ Assignments/forms-reactive/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.projectForm = new FormGroup({
-      projectName: new FormControl(null, [Validators.required, this.projectNameValidator.bind(this)]),
+      projectName: new FormControl(null, [Validators.required, this.projectNameValidator.bind(this)],
+      this.projectNameValidatorAsync.bind(this)),
       mail: new FormControl(null, [Validators.required, Validators.email]),
       status: new FormControl(null)
     });
@@ -28,5 +30,18 @@ export class AppComponent implements OnInit {
       return {'testValue': true};
     }
     return null;
+  }
+
+  projectNameValidatorAsync(control: FormControl): Promise<any> | Observer<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Test') {
+          resolve({'testValue': true});
+        } else {
+          resolve(null);
+        }
+      }, 1000);
+    });
+    return promise;
   }
 }
