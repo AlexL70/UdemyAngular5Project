a83e98c0bd88dfd8ce9aa6d948b3020e51a60dfa
AlexL70 <alexander.levinson.70@gmail.com>
2018-05-06 21:40:50 +0300

    Assignment: Practicing Reactive Forms - create form with validators

--- Assignments/forms-reactive/src/app/app.component.ts
+++ Assignments/forms-reactive/src/app/app.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectStatuses: string[] = ['Stable', 'Critical', 'Finished'];
+  projectForm: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      projectName: new FormControl(null, [Validators.required, this.projectNameValidator.bind(this)]),
+      mail: new FormControl(null, [Validators.required, Validators.email]),
+      status: new FormControl(null)
+    });
+    this.projectForm.patchValue({status: 'Stable'});
+  }
+
+  onSubmit() {
+    console.log( this.projectForm);
+  }
+
+  projectNameValidator(control: FormControl): {[s: string]: boolean} {
+    if (control.value === 'test') {
+      return {'testValue': true};
+    }
+    return null;
+  }
 }
