499df251a14cc1fa364eef537c75f1eb6fc9afc4
AlexL70 <alexander.levinson.70@gmail.com>
2018-05-27 17:44:18 +0300

    256. Creating a Signup Page and Route

--- CourseProject/src/app/app-routing.module.ts
+++ CourseProject/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full'},
@@ -15,7 +16,8 @@ const appRoutes: Routes = [
         { path: ':id', component: RecipeDetailComponent},
         { path: ':id/edit', component: RecipeEditComponent}
     ]},
-    { path: 'shopping-list', component: ShoppingListComponent}
+    { path: 'shopping-list', component: ShoppingListComponent},
+    { path: 'signup', component: SignupComponent}
 ];
 
 @NgModule({
