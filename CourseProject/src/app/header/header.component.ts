c66b8364db7112e841770afe5b605deabe17b264
AlexL70 <alexander.levinson.70@gmail.com>
2018-05-27 13:27:11 +0300

    250. Sending PUT Requests to Save Data

--- CourseProject/src/app/header/header.component.ts
+++ CourseProject/src/app/header/header.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    constructor(private dataStorage: DataStorageService) {}
+
+    onSaveData() {
+        this.dataStorage.storeRecipes().subscribe(
+            (response: Response) => console.log(response),
+            (error: Response) => console.log(error)
+        );
+    }
+}
