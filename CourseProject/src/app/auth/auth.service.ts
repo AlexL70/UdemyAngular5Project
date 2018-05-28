140261aa1404f50acb431abbf98fd80bc41c6870
AlexL70 <alexander.levinson.70@gmail.com>
2018-05-28 15:10:30 +0300

    261. Sending the Token

--- CourseProject/src/app/auth/auth.service.ts
+++ CourseProject/src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import * as firebase from 'firebase';
+import { StaticInjector } from '@angular/core/src/di/injector';
 
 export class AuthService {
+    token: string;
     signupUser(email: string, password: string) {
         firebase.auth().createUserWithEmailAndPassword( email, password)
             //  .then((response: Response) => console.log(response))
@@ -9,7 +11,18 @@ export class AuthService {
 
     signinUser(email: string, password: string) {
         firebase.auth().signInWithEmailAndPassword( email, password)
-            .then((response: Response) => console.log(response))
+            .then((response: Response) => {
+                firebase.auth().currentUser.getIdToken().then(
+                    (tk: string) => this.token = tk
+                );
+            })
             .catch(error => console.log(error));
+    }
+
+    getToken() {
+        firebase.auth().currentUser.getIdToken().then(
+            (tk: string) => this.token = tk
+        );
+        return this.token;
     }
 }
