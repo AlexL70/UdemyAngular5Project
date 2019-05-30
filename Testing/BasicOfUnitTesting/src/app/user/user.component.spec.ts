7410a0cf1ffb609fb1f03e89fdabbedc9d2d52f7
AlexL70 <alexander.levinson.70@gmail.com>
2019-05-30 18:25:33 +0300

    467. Testing Dependencies: Components and Services

--- Testing/BasicOfUnitTesting/src/app/user/user.component.spec.ts
+++ Testing/BasicOfUnitTesting/src/app/user/user.component.spec.ts
@@ -1,3 +1,4 @@
+import { UserService } from './user.service';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
@@ -5,6 +6,7 @@ import { UserComponent } from './user.component';
 describe('UserComponent', () => {
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
+  let userService: UserService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +18,28 @@ describe('UserComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserComponent);
     component = fixture.componentInstance;
+    userService = fixture.debugElement.injector.get(UserService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should use user name from the service', () => {
+    expect(userService).toBeTruthy();
+    expect(userService.user.name).toEqual(component.user.name);
+  });
+
+  it('shouldn\'t display the user name if the user is not logged in', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).not.toContain(component.user.name);
+  });
+
+  it('should display the user name if the user is logged in', () => {
+    component.isLoggedIn = true;
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).toContain(component.user.name);
   });
 });
